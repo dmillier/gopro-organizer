e781aa55979b30e41cca130d0102fd8b138ca9a5
Konrad Iturbe <github@chernowii.com>
2014-06-27 09:06:58 +0200

    Update gopro-organizer-unix.sh

--- gopro-organizer-unix.sh
+++ gopro-organizer-unix.sh
@@ -13,17 +13,16 @@ mkdir chaptered
 mkdir thumbnails
 mkdir lowresvideos
 cd ~/Desktop/myproject
-# Repeat these mv commands for each folder in DCIM
-mv /path/to/DCIM/XXXGOPRO/*.MP4 videos
-mv /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.JPG photos
-mv /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
-mv /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
+# Repeat these cp commands for each folder in DCIM
+cp /path/to/DCIM/XXXGOPRO/*.MP4 videos
+cp /path/to/DCIM/XXXGOPRO/*.JPG photos
+cp /path/to/DCIM/XXXGOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
+cp /path/to/DCIM/XXXGOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
 cd photos
-mv GOPR*.JPG single
-mv G*.JPG timelapse-burst-continuous
+cp GOPR*.JPG single
+cp G*.JPG timelapse-burst-continuous
 cd ~/Desktop/myproject
 cd videos
-mv GOPR*.MP4 single
-#Delete the hash that is at the left of mv if you have chaptered videos
-#mv GP*.MP4 chaptered
+cp GOPR*.MP4 single
+cp GP*.MP4 chaptered
 echo GoPro Organizer has finished to sort all your epic videos and your awesome photos
