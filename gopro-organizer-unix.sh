4656e5eeb6d4578cdcc31da41b4a55897a33a207
Konrad Iturbe <github@chernowii.com>
2014-06-27 09:20:05 +0200

    Update gopro-organizer-unix.sh

--- gopro-organizer-unix.sh
+++ gopro-organizer-unix.sh
@@ -1,27 +1,27 @@
 cd ~/Desktop
-mkdir myproject
-cd myproject
+mkdir goprohero
+cd goprohero
 mkdir videos
 mkdir photos
 cd photos
 mkdir single
 mkdir timelapse-burst-continuous
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 cd videos
 mkdir single
 mkdir chaptered
 mkdir thumbnails
 mkdir lowresvideos
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 # Repeat these cp commands for each folder in DCIM
 cp /path/to/DCIM/XXXGOPRO/*.MP4 videos
 cp /path/to/DCIM/XXXGOPRO/*.JPG photos
-cp /path/to/DCIM/XXXGOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
-cp /path/to/DCIM/XXXGOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
+cp /path/to/DCIM/XXXGOPRO/*.THM ~/Desktop/goprohero/videos/thumbnails
+cp /path/to/DCIM/XXXGOPRO/*.LRV ~/Desktop/goprohero/videos/lowresvideos
 cd photos
 cp GOPR*.JPG single
 cp G*.JPG timelapse-burst-continuous
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 cd videos
 cp GOPR*.MP4 single
 cp GP*.MP4 chaptered
