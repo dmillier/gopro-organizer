5c950fe502f4d9dbee9b377987d3cae42e575397
Konrad Iturbe <github@chernowii.com>
2014-06-27 09:18:42 +0200

    Update gopro-organizer-win.sh

--- gopro-organizer-win.sh
+++ gopro-organizer-win.sh
@@ -1,3 +1,10 @@
+#######     ##########  #########   #########   #########
+##          ##      ##  ###     ##  ####    ##  ##     ##
+##  #####   ##      ##  ###     ##  ####    ##  ##     ##
+##     ##   ##      ##  #########   #########   ##     ##
+##     ##   ##      ##  ####        #######     ##     ##
+#########   ##########  ####        #####  ##   #########
+
 cd ~/Desktop
 md myproject
 cd myproject
@@ -13,17 +20,16 @@ md chaptered
 md thumbnails
 md lowresvideos
 cd ~/Desktop/myproject
-# Repeat these move commands for each folder in DCIM
-move /path/to/DCIM/XXXGOPRO/*.MP4 videos
-move /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.JPG photos
-move /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
-move /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
+# Repeat these cp commands for each folder in DCIM
+cp /path/to/DCIM/XXXGOPRO/*.MP4 videos
+cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.JPG photos
+cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
+cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
 cd photos
-move GOPR*.JPG single
-move G*.JPG timelapse-burst-continuous
+cp GOPR*.JPG single
+cp G*.JPG timelapse-burst-continuous
 cd ~/Desktop/myproject
 cd videos
-move GOPR*.MP4 single
-#Delete the hash that is at the left of move if you have chaptered videos
-#move GP*.MP4 chaptered
+cp GOPR*.MP4 single
+cp GP*.MP4 chaptered
 echo GoPro Organizer has finished to sort all your epic videos and your awesome photos
