804b13ada9c8639756f95de0fc9b9489737fc84e
Konrad Iturbe <github@chernowii.com>
2014-06-27 09:20:33 +0200

    Update gopro-organizer-win.sh

--- gopro-organizer-win.sh
+++ gopro-organizer-win.sh
@@ -1,27 +1,27 @@
 cd ~/Desktop
-md myproject
-cd myproject
+md goprohero
+cd goprohero
 md videos
 md photos
 cd photos
 md single
 md timelapse-burst-continuous
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 cd videos
 md single
 md chaptered
 md thumbnails
 md lowresvideos
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 # Repeat these cp commands for each folder in DCIM
 cp /path/to/DCIM/XXXGOPRO/*.MP4 videos
 cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.JPG photos
-cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.THM ~/Desktop/myproject/videos/thumbnails
-cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.LRV ~/Desktop/myproject/videos/lowresvideos
+cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.THM ~/Desktop/goprohero/videos/thumbnails
+cp /media/konraditurbe/8765-4321/DCIM/100GOPRO/*.LRV ~/Desktop/goprohero/videos/lowresvideos
 cd photos
 cp GOPR*.JPG single
 cp G*.JPG timelapse-burst-continuous
-cd ~/Desktop/myproject
+cd ~/Desktop/goprohero
 cd videos
 cp GOPR*.MP4 single
 cp GP*.MP4 chaptered
